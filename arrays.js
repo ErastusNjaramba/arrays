f8e3aeedc88c3b98aef8196038b2eed5d7e98f6f
ErastusNjaramba <erastus11njaramba@gmail.com>
2024-07-16 11:58:09 +0300

    arrays

---
+++ arrays.js
@@ -0,0 +1,7 @@
+let fruits = ['banana', 'apple', 'kiwi', 'cherry', 20, true, false, 200];
+fruits[1] = "avacado";
+console.log(fruits[0])
+console.log(fruits[1])
+console.log(fruits[2])
+console.log(fruits[3])
+console.log(fruits[4])
